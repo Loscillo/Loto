2c38fcb8b3fc010d727d12a48685d946ec3b03a1
GibbsFrance <ngaillard.vosges@gmail.com>
2012-11-16 14:57:29 +0100

    Dev branch for multiple loto

---
+++ src/fr/jules_cesar/Loto/Loto.java
@@ -0,0 +1,16 @@
+package fr.jules_cesar.Loto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loto {
+	public List<Integer> valeur_id = new ArrayList<Integer>();
+	public long delai = 1200L;
+	public boolean protection = true;
+	public boolean announce = false;
+	public String joueur = null;
+	
+	public void modify(int id, String container, Object value){
+		
+	}
+}
